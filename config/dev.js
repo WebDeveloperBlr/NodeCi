198654026651c914a046210b5c05e1e62c634d92
Dima Korolev <kdv3505381@gmail.com>
2018-08-17 17:18:26 +0300

    mobgo problem

--- config/dev.js
+++ config/dev.js
@@ -2,8 +2,8 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+  mongoURI: 'mongodb://mongodb',
   cookieKey: '123123123',
-  mongoTestURI: 'mongodb://127.0.0.1:27017/blog_ci',
+  mongoTestURI: 'mongodb://localhost',
   redisUrl: 'redis://redis',
 };
